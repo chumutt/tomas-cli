62a99651d5e7c13be9ef3b726df35f67bcbac725
ChuMutt <chufilthymutt@gmail.com>
2024-02-26 20:56:37 -0600

    fix

--- run.lisp
+++ run.lisp
@@ -30,6 +30,7 @@ Another solution to run the app is to build and run a binary (see README).
 (in-package #:tomas-cli)
 
 (handler-case
+    (launch)
   (error (c)
     (format *error-output* "~&An error occured: ~a~&" c)
     (uiop:quit 1)))
