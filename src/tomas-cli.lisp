b7d9aa7766c3e87e8eeab8ecfb55661cf60cda03
ChuMutt <chufilthymutt@gmail.com>
2024-03-09 20:16:57 -0600

    bleh

--- src/tomas-cli.lisp
+++ src/tomas-cli.lisp
@@ -2,6 +2,23 @@
 
 ;; Define your project functionality here...
 
+(defvar *playtime-epoch* nil)
+
+(defun get-when-loaded ()
+  (load-time-value (local-time:now)))
+
+(defun set-when-loaded ()
+  (setf *playtime-epoch* (get-when-loaded)))
+
+(defvar *pid* nil)
+
+(defvar *pets* nil)
+
+(defun get-pid ()
+  (uiop:getenv "USER"))
+
+(defun set-pid ()
+  (setf *pid* (get-pid)))
 
 (defun greet (&optional (name "chu the pup"))
   (format T "Hello ~a from ~a!~&" name "tomas"))
@@ -11,6 +28,10 @@
 
   tomas [name]~&"))
 
+(defun startup-setup ()
+  (set-when-loaded)
+  (set-pid))
+
 (defun %main (argv)
   "Parse CLI args."
   (when (member "-h" argv :test #'equal)
@@ -18,6 +39,7 @@
     ;; clingon, unix-opts, defmain, adopt… when needed.
     (help)
     (uiop:quit))
+  (startup-setup)
   (greet (or (first argv)
              "dear lisp user")))
 
